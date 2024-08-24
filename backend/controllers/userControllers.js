f6bf07666d70d59ddd4a6252dbb100f47d97d8e3
rhedwan <adesolaridwan2003@gmail.com>
2024-08-24 01:47:43 +0100

    feat: hashing password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,15 +3,15 @@ const User = require("../models/userModel");
 
 exports.createUser = catchAsync(async (req, res, next) => {
   console.log(req.body);
-  // const newUser = await User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  //   passwordConfirm: req.body.passwordConfirm,
-  // });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(200).json({
     data: "success",
-    // user: newUser,
+    user: newUser,
   });
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -37,6 +38,18 @@ const userSchema = new mongoose.Schema({
     default: false,
     select: false,
   },
+  dateOfBirth: Date,
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Others"],
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model("User", userSchema);
