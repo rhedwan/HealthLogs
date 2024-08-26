866abc944d745cfb96a3011b56614fb73bfa00c9
rhedwan <adesolaridwan2003@gmail.com>
2024-08-26 12:44:46 +0100

    feat: update user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -51,3 +51,25 @@ exports.getAllPatients = catchAsync(async (req, res, next) => {
     patients,
   });
 });
+
+const filteredObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+exports.updateUser = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm)
+    return next(new AppError("This route is not for password updates", 400));
+  const filteredBody = filteredObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: updatedUser,
+  });
+});
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,6 +4,7 @@ const {
   createUser,
   login,
   getAllPatients,
+  updateUser,
 } = require("../controllers/userControllers");
 const {
   protect,
@@ -20,6 +21,7 @@ router.post("/login", login);
 router.post("/forgetPassword", forgetPassword);
 router.patch("/resetPassword/:token", resetPassword);
 router.patch("/updatePassword", protect, updatePassword);
+router.patch("/updateUser", protect, updateUser);
 router.get("/", protect, restrictTo("admin", "patient"), getAllPatients);
 
 module.exports = router;
