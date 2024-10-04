c3d9dfd52673494355f02736dfb0608e95031c6d
letami0 <letam@izero.dev>
2024-10-04 08:22:54 +0100

    feat: select box reaction

--- healthlogs-frontend/schema/PatientAllergy.ts
+++ healthlogs-frontend/schema/PatientAllergy.ts
@@ -10,3 +10,34 @@ export interface PatientAllergy {
   status: string;
   __v: number;
 }
+
+export const reactionList = [
+  "Unknown",
+  "Anaphylaxis",
+  "Bloating/gas",
+  "Bradycardia",
+  "Chest Pain",
+  "Conjunctivitis",
+  "Cough",
+  "Diarrhea",
+  "Difficulty speaking or swallowing",
+  "Dizziness/Lightheadedness",
+  "Facial swelling",
+  "Hives",
+  "Irregular Heartbeat",
+  "Itchiness",
+  "Loss of consciousness",
+  "Nausea",
+  "Pain/cramping",
+  "Patchy swelling-skin",
+  "Rash - generalized",
+  "Rash - localized",
+  "Respiratory Distress",
+  "Runny nose",
+  "Shortness of breath",
+  "Tachycardia",
+  "Tongue swelling",
+  "Vomiting",
+  "Wheezing",
+  "Other",
+];
